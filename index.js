d4f2ad37669255d345d88b9932659b16c3eb4212
President <aaron.m.wilson@uconn.edu>
2022-07-17 19:21:44 -0400

    added questions in index.js using inquirer

--- index.js
+++ index.js
@@ -2,7 +2,39 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'projectTitle',
+                            message: 'What is your project title?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'description',
+                            message: 'How would you describe your project?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'installationInstructions',
+                            message: 'What steps do you take to install your application?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'usageInfo',
+                            message: 'How do you use your application?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'contributionGuide',
+                            message: 'What are the guidelines for contribution?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'testInstruction',
+                            message: 'How can you test the application?',
+                        }
+                    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
