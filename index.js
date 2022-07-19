3d4e4869fcec03abcb35dc32efffda2a719e15f6
President <aaron.m.wilson@uconn.edu>
2022-07-19 14:18:26 -0400

    changed questions into a function

--- index.js
+++ index.js
@@ -3,52 +3,48 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils')
 // TODO: Create an array of questions for user input
-const questions = inquirer
-                    .prompt([
-                        {
-                            type: 'input',
-                            name: 'projectTitle',
-                            message: 'What is your project title?',
-                            // validates input
-                            validate: (value)=>{ if(value){return true} else {return 'please input a project title to continue'}}
-                        },
-                        {
-                            type: 'input',
-                            name: 'description',
-                            message: 'How would you describe your project?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'installationInstructions',
-                            message: 'What steps do you take to install your application?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'usageInfo',
-                            message: 'How do you use your application?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'contributionGuide',
-                            message: 'What are the guidelines for contribution?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'testInstruction',
-                            message: 'How can you test the application?',
-                        }
-                    ]) 
-                    .then((data => {
-                        
-                        Answers = data
-                        console.log(Answers)
-                        return Answers
-                    }))
-                    
-                    
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is your project title?',
+            // validates input
+            validate: (value)=>{ if(value){return true} else {return 'please input a project title to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'How would you describe your project?',
+        },
+        {
+            type: 'input',
+            name: 'installationInstructions',
+            message: 'What steps do you take to install your application?',
+        },
+        {
+            type: 'input',
+            name: 'usageInfo',
+            message: 'How do you use your application?',
+        },
+        {
+            type: 'input',
+            name: 'contributionGuide',
+            message: 'What are the guidelines for contribution?',
+        },
+        {
+            type: 'input',
+            name: 'testInstruction',
+            message: 'How can you test the application?',
+        },
+    ]);
+};
 
+                    
+                    
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+
 
 
 
@@ -56,7 +52,10 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 
+
 // Function call to initialize app
-init()
+init(
+    questions()
+)
 
     
